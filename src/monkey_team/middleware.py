eca26958c57c4c0c68b7043898c6dccf6efaf1ec
Ionel Maries Cristian <contact@ionelmc.ro>
2012-11-03 01:14:17 +0200

    Use a template for the error response.

--- src/monkey_team/middleware.py
+++ src/monkey_team/middleware.py
@@ -4,22 +4,10 @@ import os
 from Crypto.Cipher import AES
 
 from django.views.debug import technical_500_response
+from django.template.loader import render_to_string
 
 from .admin import MonkeySetup
 
-RESPONSE_BODY = (
-    '<html><!-- %(client_key)s --><head>'
-        '<meta http-equiv="content-type" content="text/html; charset=UTF-8">'
-        '<title>500 Internal Server Error</title>'
-    '</head>'
-    '<body>'
-    '<h1>500 Internal Server Error</h1>'
-    '<p>Sorry, something went wrong.<br><br>A team of highly trained monkeys has been dispatched to deal with this situation.</p>'
-    'If you see them, show them this information:<br>'
-    '<pre>%(data)s</pre>'
-    '<script>%(extra)s</script>'
-    '</body></html>'
-)
 BLOCK_SIZE = 16
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
 unpad = lambda s : s[0:-ord(s[-1])]
@@ -29,7 +17,7 @@ class MonkeyTeamMiddleware(object):
     @staticmethod
     def patch_response(request, response):
         iv = os.urandom(16)
-        response.content = RESPONSE_BODY % {
+        response.content = render_to_string("monkey_500.html", {
             'client_key': MonkeySetup.client_key,
             'data': (
                 iv + AES.new(
@@ -41,7 +29,7 @@ class MonkeyTeamMiddleware(object):
                 )
             ).encode('base64'),
             'extra': ''#MonkeySetup.get_userscript_code(request),
-        }
+        })
 
     def process_exception(self, request, exception):
         exc_info = sys.exc_info()
